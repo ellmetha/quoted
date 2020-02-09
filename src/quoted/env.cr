c82fadd95e26e3eaf9b0208e35be3876816b89c1
Morgan Aubert <morgan@aubert.email>
2020-02-09 14:38:05 -0500

    Add basic env helper

---
+++ src/quoted/env.cr
@@ -0,0 +1,22 @@
+module Quoted
+  class Env
+    def ==(value)
+      env_id == value
+    end
+
+    macro method_missing(call)
+      {% if call.name.ends_with?('?') %}
+        return self.==("{{ call.name[0..-2] }}")
+      {% end %}
+      super
+    end
+
+    private def env_id
+      @env_id = ENV["KEMAL_ENV"]? || "development"
+    end
+  end
+
+  def self.env
+    @@env ||= Env.new
+  end
+end
