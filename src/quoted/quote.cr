da72ed7c7bc62e4c8eb0a2edb1b970ae06b1add4
Morgan Aubert <morgan@aubert.email>
2020-02-11 21:43:11 -0500

    Start Quote#random method

--- src/quoted/quote.cr
+++ src/quoted/quote.cr
@@ -5,6 +5,17 @@ module Quoted
     property large_image_url : String
     property medium_image_url : String
 
+    def self.random
+      api_client = ApiClient::FavQs.new
+      quote = api_client.qotd
+      new(
+        body: quote["quote"]["body"].as_s,
+        author: quote["quote"]["author"].as_s,
+        large_image_url: "https://example.com",
+        medium_image_url: "https://example.com"
+      )
+    end
+
     def initialize(@body, @author, @large_image_url, @medium_image_url)
     end
   end
