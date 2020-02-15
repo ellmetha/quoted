78ad11fdb6952c66b86a026e14410bf8e0cdc6b1
Morgan Aubert <morgan@aubert.email>
2020-02-15 13:31:53 -0500

    Expose random quote information in main route and add debug display

--- src/quoted/server.cr
+++ src/quoted/server.cr
@@ -2,6 +2,7 @@ require "kemal"
 
 module Quoted
   get "/" do
+    quote = Quote.random # ameba:disable Lint/UselessAssign
     render "src/views/quote.ecr", "src/views/layouts/base.ecr"
   end
 end
